624bc3a5b29c373cf8952a0cb466be974e5fcbd1
mdansarulislam641 <mdansarulislam641.com>
2023-03-11 09:56:44 +0600

    layout and ui design done

---
+++ src/components/Transactions/Transaction.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import editImage from '../../assets/images/edit.svg';
+import deleteImage from '../../assets/images/delete.svg';
+const Transaction = () => {
+    return (
+        <li className="transaction income">
+        <p>Earned this month</p>
+        <div className="right">
+            <p>৳ 100</p>
+            <button className="link">
+                <img
+                alt='edit'
+                    className="icon"
+                    src={editImage}
+                />
+            </button>
+            <button className="link">
+                <img alt='delete'
+                    className="icon"
+                    src={deleteImage}
+                />
+            </button>
+        </div>
+    </li>
+    );
+};
+
+export default Transaction;
